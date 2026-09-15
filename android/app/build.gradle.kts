cac5a1a9ff1685357369fe945c4bd1c354ac1bb2
ahmed5330 <mr.a.mnaa@gmail.com>
2026-09-15 22:24:55 +0300

    Complete Android build dependencies and tests

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,5 +13,20 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
 }
