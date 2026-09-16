527b40c4f00098565eda4cd6330a19e429ad5166
ahmed5330 <mr.a.mnaa@gmail.com>
2026-09-16 22:52:08 +0300

    Bump Android to v2.3.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kunonline.callerid.stable"
         minSdk = 29
         targetSdk = 35
-        versionCode = 105
-        versionName = "2.3.0"
+        versionCode = 106
+        versionName = "2.3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
