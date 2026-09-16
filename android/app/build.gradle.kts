0c183167717b96f6993deba161268bedc857c3d3
ahmed5330 <mr.a.mnaa@gmail.com>
2026-09-16 14:46:31 +0300

    Add coroutines for native mobile data loading

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
     testImplementation("junit:junit:4.13.2")
 }
