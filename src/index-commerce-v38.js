3fa18f0b2571b4dbdfb53d1d4cf7d2dafd392128
ahmed5330 <mr.a.mnaa@gmail.com>
2026-09-25 00:09:06 +0300

    Bump idempotent collaboration sidebar asset

--- src/index-commerce-v38.js
+++ src/index-commerce-v38.js
@@ -18,7 +18,7 @@ const V2_UI_SCRIPTS=[
   '<script src="/v2/modules-v109-operational-date-contact.js?v=109.1" data-kun-operational-date-contact="1"></script>',
   '<script src="/v2/modules-v117-collaboration-root.js?v=117.0" data-kun-collaboration-root="1"></script>',
   '<script src="/v2/modules-v117-collaboration.js?v=117.0" data-kun-collaboration="1"></script>',
-  '<script src="/v2/modules-v117-collaboration-sidebar.js?v=117.1" data-kun-collaboration-sidebar="1"></script>'
+  '<script src="/v2/modules-v117-collaboration-sidebar.js?v=117.2" data-kun-collaboration-sidebar="1"></script>'
 ].join('');
 
 function redirectLegacyRoot(request){
