eb03544d84f876e2275c93be321440821beff626
ahmed5330 <mr.a.mnaa@gmail.com>
2026-09-24 21:20:59 +0300

    Load collaboration root guard before team UI

--- src/index-commerce-v38.js
+++ src/index-commerce-v38.js
@@ -15,6 +15,7 @@ const V2_UI_SCRIPTS=[
   '<script src="/v2/modules-v106-manual-jnt-order.js?v=106.0" data-kun-manual-jnt-order="1"></script>',
   '<script src="/v2/modules-v107-mobile-app-update.js?v=107.0" data-kun-mobile-app-update="1"></script>',
   '<script src="/v2/modules-v109-operational-date-contact.js?v=109.1" data-kun-operational-date-contact="1"></script>',
+  '<script src="/v2/modules-v117-collaboration-root.js?v=117.0" data-kun-collaboration-root="1"></script>',
   '<script src="/v2/modules-v117-collaboration.js?v=117.0" data-kun-collaboration="1"></script>'
 ].join('');
 
